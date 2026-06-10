04b7f89fbb61c2ad9ba01bd34130028376e2a728
Arkia <arkia456.dev@gmail.com>
2026-06-10 11:46:36 +0200

    Config: Move playlist and shopping list files path to env

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -8,8 +7,11 @@ load_dotenv()
 FREE_MOBILE_USER = os.getenv("FREE_MOBILE_USER", "")
 FREE_MOBILE_API_KEY = os.getenv("FREE_MOBILE_API_KEY", "")
 
-if sys.platform == "win32":
-    PLAYLIST_FILES = {
-      "MADNESS": Path(r"C:\Users\Aurore\Music\Playlists\Madness.m3u8")
-		}
-    SHOPPING_LIST_FILE = Path(r"C:\Users\Aurore\Documents\Documents\shopping_list.txt")
+PLAYLIST_FILES = {
+    key[len("PLAYLIST_"):]: Path(value)
+    for key, value in os.environ.items()
+    if key.startswith("PLAYLIST_") and value
+}
+
+_shopping_list = os.getenv("SHOPPING_LIST_FILE", "")
+SHOPPING_LIST_FILE = Path(_shopping_list) if _shopping_list else None
